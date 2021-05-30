c347422102d0761591765d9a48d32a6bb4f68614
Johan Camilo Lopez <camilino123@gmail.com>
2021-05-29 20:42:30 -0500

    Spring project Skeleton Added

---
+++ src/test/java/com/isladellago/watercalculator/WatercalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isladellago.watercalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatercalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
