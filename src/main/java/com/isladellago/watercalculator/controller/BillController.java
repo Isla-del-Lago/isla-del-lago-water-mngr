b797ce2819b22d65c9e58c694b27734f0e8d5e47
Johan Camilo Lopez <camilino123@gmail.com>
2021-05-30 21:24:12 -0500

    Bill controller and service created

---
+++ src/main/java/com/isladellago/watercalculator/controller/BillController.java
@@ -0,0 +1,9 @@
+package com.isladellago.watercalculator.controller;
+
+/**
+ * This class is responsible to hold all paths to handle requests
+ * over bill entity.
+ */
+public class BillController {
+
+}
---
+++ src/main/java/com/isladellago/watercalculator/service/impl/BillServiceImpl.java
@@ -0,0 +1,6 @@
+package com.isladellago.watercalculator.service.impl;
+
+import com.isladellago.watercalculator.service.BillService;
+
+public class BillServiceImpl implements BillService {
+}
